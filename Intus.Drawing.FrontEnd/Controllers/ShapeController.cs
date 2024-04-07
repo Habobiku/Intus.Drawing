f0ebcec94cb782e29cdcd3e45a698029c7b59b18
gireyosmanov <girey345@gmail.com>
2024-04-07 18:39:17 +0300

    created shape controller

---
+++ Intus.Drawing.FrontEnd/Controllers/ShapeController.cs
@@ -0,0 +1,26 @@
+﻿using Intus.Drawing.Persistence.Entities;
+using Intus.Drawing.Persistence.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intus.Drawing.FrontEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ShapeController : ControllerBase
+	{
+        private readonly IShapeService _shapeService;
+
+        public ShapeController(IShapeService shapeService)
+        {
+            _shapeService = shapeService ?? throw new ArgumentNullException(nameof(shapeService));
+        }
+
+        [HttpGet("rectangle")]
+        public async Task<ActionResult> GetRectangle()
+        {
+            return Ok(await _shapeService.GetFigureFromJson<Rectangle>());
+        }
+    }
+}
+
